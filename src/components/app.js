0f078b09e44762431d5243d672f79cbaf25d435d
Jasmine <jquek@thoughtworks.com>
2016-05-09 15:34:27 +0800

    Basic contact details form implemented

--- src/components/app.js
+++ src/components/app.js
@@ -5,19 +5,121 @@ import ReactDOM from 'react-dom';
 let App = React.createClass({
   render() {
     return (
-      <div className="lettersToTheEditor">
-        <h1>IT WORKS!!</h1>
-        <PersonalDetails/>
+      <div className="lettersToTheEditor container">
+        <div className="row">
+          <h1 className="display-1">Letters to the Editor</h1>
+          <PersonalDetails/>
+        </div>
       </div>
     );
   }
 });
 
 let PersonalDetails = React.createClass({
+  getInitialState() {
+    return {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+      address: '',
+      postcode: ''
+    };
+  },
+
+  handleChange(key) {
+    return function(e) {
+      const state = {};
+
+      state[key] = e.target.value;
+      this.setState(state);
+    }.bind(this);
+  },
+
+  handleSubmit(e) {
+    e.preventDefault();
+    // handle data
+    // send to server
+    console.log(this.state);
+  },
+
   render() {
     return (
-      <div className="personal">
-      </div>
+      <form className="form-horizontal" onSubmit={this.handleSubmit}>
+        <div className="form-group row">
+          <label className="col-sm-2 form-control-label" htmlFor="firstName">First Name</label>
+          <div className="col-sm-4">
+            <input
+              className="form-control"
+              id="firstName"
+              required type="text"
+              value={this.state.firstName}
+              onChange={this.handleChange('firstName')}
+            />
+          </div>
+        </div>
+        <div className="form-group row">
+          <label className="col-sm-2 form-control-label" htmlFor="lastName">Last Name</label>
+          <div className="col-sm-4">
+            <input
+              className="form-control"
+              id="lastName"
+              required type="text"
+              value={this.state.lastName}
+              onChange={this.handleChange('lastName')}
+            />
+          </div>
+        </div>
+        <div className="form-group row">
+          <label className="col-sm-2 form-control-label" htmlFor="email">Email</label>
+          <div className="col-sm-4">
+            <input
+              className="form-control"
+              id="email"
+              required type="email"
+              value={this.state.email}
+              onChange={this.handleChange('email')}
+            />
+          </div>
+        </div>
+        <div className="form-group row">
+          <label className="col-sm-2 form-control-label" htmlFor="phone">Phone</label>
+          <div className="col-sm-4">
+            <input
+              className="form-control"
+              id="phone"
+              required type="phone"
+              value={this.state.phone}
+              onChange={this.handleChange('phone')}
+            />
+          </div>
+        </div>
+        <div className="form-group row">
+          <label className="col-sm-2 form-control-label" htmlFor="address">Address</label>
+          <div className="col-sm-4">
+            <input
+              className="form-control"
+              id="address"
+              required type="address"
+              value={this.state.address}
+              onChange={this.handleChange('address')}
+            />
+          </div>
+        </div>
+        <div className="form-group row">
+          <label className="col-sm-2 form-control-label" htmlFor="postcode">Post code</label>
+          <div className="col-sm-2">
+            <input
+              className="form-control"
+              id="postcode"
+              required type="postcode"
+              value={this.state.postcode}
+              onChange={this.handleChange('postcode')}
+            />
+          </div>
+        </div>
+        <input className="btn btn-primary" type="submit" value="Submit"/>
+      </form>
     );
   }
 });
