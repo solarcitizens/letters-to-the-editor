7caf603b21a89a3e0dbb6a816492bf7baee89844
Robin Doherty <rdoherty@gmail.com>
2016-05-11 16:22:31 +0930

    Revert "remove test to make build pass"
    This reverts commit 5d7549e710fc7cc3d62477ebee9c25f145cd3d6a.

--- src/components/__tests__/app_test.js
+++ src/components/__tests__/app_test.js
@@ -4,10 +4,9 @@ import { App } from '../app';
 
 describe('App', () => {
   it('does stuff', () => {
-    expect(1).toEqual(1);
-    // const form = shallowRender(<App/>);
+    const form = shallowRender(<App/>);
 
-    // expect(app.props.children[0].type).toEqual('h1');
-    // expect(app.props.children[1].props.children).toEqual('It works!');
+    expect(app.props.children[0].type).toEqual('h1');
+    expect(app.props.children[1].props.children).toEqual('It works!');
   });
 });
