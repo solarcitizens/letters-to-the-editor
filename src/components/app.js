c2e2f54e71cfc1dc2e44af2e433292477f6f7996
Robin Doherty <rdoherty@gmail.com>
2016-05-11 20:14:26 +0930

    fix most eslint errors

--- src/components/app.js
+++ src/components/app.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import PersonalDetailFields from './personalDetailFields.js';
 
-export default class App extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       fieldValues: props.formValues,
+      handleChange: this.handleChange.bind(this),
+      handleSubmit: this.handleSubmit.bind(this),
     };
   }
 
-  onChange(fieldName) {
+  handleSubmit() {
+    // send stuff to server
+  }
+
+  handleChange(fieldName) {
     const detailsComponent = this;
 
     return event => {
       const newFieldValues = Object.assign({}, detailsComponent.state.fieldValues, { [fieldName]: event.target.value });
+
       detailsComponent.setState({ fieldValues: newFieldValues });
     };
   }
-
-  submitDetails() {
-    //submitting happens
-  }
+  // handleChange(key) {
+  //   return function update(e) {
+  //     const state = {};
+  //
+  //     state[key] = e.target.value;
+  //     this.setState(state);
+  //   }.bind(this);
+  // }
 
   render() {
     return (
@@ -29,12 +40,20 @@ export default class App extends React.Component {
           <h1 className="display-1">Letters to the Editor</h1>
           <div className="form-body">
             <PersonalDetailFields
-              onChange={this.onChange.bind(this)}
-              formValues={this.props.formValues} />
-            <button onClick={this.submitDetails.bind(this)}>Register</button>
+              formValues={this.state.fieldValues}
+              onChange={this.handleChange}
+              onSubmit={this.handleSubmit}
+            />
           </div>
         </div>
       </div>
     );
   }
+
 }
+
+App.propTypes = {
+  formValues: React.PropTypes.object,
+};
+
+export default App;
