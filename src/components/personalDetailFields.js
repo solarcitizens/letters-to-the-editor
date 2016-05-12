4f0b2d6ae97b1e0013d65b34f2cf95f254738b1d
Robin Doherty <rdoherty@gmail.com>
2016-05-12 23:27:59 +1000

    remove unhelpful shorthand-first eslint

--- src/components/personalDetailFields.js
+++ src/components/personalDetailFields.js
@@ -11,90 +11,92 @@ import React from 'react';
 
 const PersonalDetailFields = props =>
  (
-  <PersonalDetailFields>
-    <form className="form-horizontal" onSubmit={props.onSubmit}>
-      <div className="form-group row">
-        <label className="col-sm-2 form-control-label" htmlFor="firstName">First Name</label>
-        <div className="col-sm-4">
-          <input
-            className="form-control"
-            defaultValue={props.formValues.firstName}
-            id="firstName"
-            required type="text"
-            onChange={props.onChange('firstName')}
-          />
-        </div>
+  <form className="form-horizontal" onSubmit={props.onSubmit}>
+    <div className="form-group row">
+      <label className="col-sm-2 form-control-label" htmlFor="firstName">First Name</label>
+      <div className="col-sm-4">
+        <input
+          className="form-control"
+          defaultValue={props.formValues.firstName}
+          id="firstName"
+          required type="text"
+          onChange={props.onChange('firstName')}
+        />
       </div>
-      <div className="form-group row">
-        <label className="col-sm-2 form-control-label" htmlFor="lastName">Last Name</label>
-        <div className="col-sm-4">
-          <input
-            className="form-control"
-            id="lastName"
-            required type="text"
-            value={props.formValues.lastName}
-            onChange={props.onChange('lastName')}
-          />
-        </div>
+    </div>
+    <div className="form-group row">
+      <label className="col-sm-2 form-control-label" htmlFor="lastName">Last Name</label>
+      <div className="col-sm-4">
+        <input
+          className="form-control"
+          id="lastName"
+          required type="text"
+          value={props.formValues.lastName}
+          onChange={props.onChange('lastName')}
+        />
       </div>
-      <div className="form-group row">
-        <label className="col-sm-2 form-control-label" htmlFor="email">Email</label>
-        <div className="col-sm-4">
-          <input
-            className="form-control"
-            id="email"
-            required type="email"
-            value={props.formValues.email}
-            onChange={props.onChange('email')}
-          />
-        </div>
+    </div>
+    <div className="form-group row">
+      <label className="col-sm-2 form-control-label" htmlFor="email">Email</label>
+      <div className="col-sm-4">
+        <input
+          className="form-control"
+          id="email"
+          required type="email"
+          value={props.formValues.email}
+          onChange={props.onChange('email')}
+        />
       </div>
-      <div className="form-group row">
-        <label className="col-sm-2 form-control-label" htmlFor="phone">Phone</label>
-        <div className="col-sm-4">
-          <input
-            className="form-control"
-            id="phone"
-            required type="phone"
-            value={props.formValues.phone}
-            onChange={props.onChange('phone')}
-          />
-        </div>
+    </div>
+    <div className="form-group row">
+      <label className="col-sm-2 form-control-label" htmlFor="phone">Phone</label>
+      <div className="col-sm-4">
+        <input
+          className="form-control"
+          id="phone"
+          required type="phone"
+          value={props.formValues.phone}
+          onChange={props.onChange('phone')}
+        />
       </div>
-      <div className="form-group row">
-        <label className="col-sm-2 form-control-label" htmlFor="address">Address</label>
-        <div className="col-sm-4">
-          <input
-            className="form-control"
-            id="address"
-            required type="address"
-            value={props.formValues.address}
-            onChange={props.onChange('address')}
-          />
-        </div>
+    </div>
+    <div className="form-group row">
+      <label className="col-sm-2 form-control-label" htmlFor="address">Address</label>
+      <div className="col-sm-4">
+        <input
+          className="form-control"
+          id="address"
+          required type="address"
+          value={props.formValues.address}
+          onChange={props.onChange('address')}
+        />
       </div>
-      <div className="form-group row">
-        <label className="col-sm-2 form-control-label" htmlFor="postcode">Post code</label>
-        <div className="col-sm-2">
-          <input
-            className="form-control"
-            id="postcode"
-            required type="postcode"
-            value={props.formValues.postcode}
-            onChange={props.onChange('postcode')}
-          />
-        </div>
+    </div>
+    <div className="form-group row">
+      <label className="col-sm-2 form-control-label" htmlFor="postcode">Post code</label>
+      <div className="col-sm-2">
+        <input
+          className="form-control"
+          id="postcode"
+          required type="postcode"
+          value={props.formValues.postcode}
+          onChange={props.onChange('postcode')}
+        />
       </div>
-      <input className="btn btn-primary" type="submit" value="Submit"/>
-    </form>
-  </PersonalDetailFields>
+    </div>
+    <input className="btn btn-primary" type="submit" value="Submit"/>
+  </form>
 );
 
 
+// PersonalDetailFields.defaultProps = {
+//   formValues: {},
+// }
+
 PersonalDetailFields.propTypes = {
-  formValues: React.PropTypes.object,
-  onChange: React.PropTypes.func,
-  onSubmit: React.PropTypes.func,
+  formValues: React.PropTypes.object.isRequired,
+  onChange: React.PropTypes.func.isRequired,
+  onSubmit: React.PropTypes.func.isRequired,
 };
 
 export default PersonalDetailFields;
