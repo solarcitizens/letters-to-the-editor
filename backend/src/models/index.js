7e55c7da4a3096f46e82ef6480a510d9983ae95f
Robin Doherty <rdoherty@gmail.com>
2016-05-21 20:59:06 +0930

    configure database connection by env var for staging and prod

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -12,7 +12,7 @@ const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../../config/db.json')[env];
 
-let connection = new Sequelize(config.database, config.username, config.password, config);
+let connection = require(__dirname + '/../db/connection.js');
 
 console.log("Path to letter model: " + __dirname + "/letter.js");
 let Letter = connection.import(__dirname + "/letter.js");
