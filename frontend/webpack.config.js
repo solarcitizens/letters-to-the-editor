1284f2491d1df3630c322805463cadf0eaa67726
Robin Doherty <rdoherty@gmail.com>
2016-05-21 22:53:57 +0930

    copy assets to backend after webpacking

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,7 +2,8 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: './src/index.js',
   output: {
-    path: __dirname + '/../backend/public/javascript',
+    path: `${__dirname}/public/javascript`,
+    publicPath: 'javascript',
     filename: 'ltte.bundle.js',
   },
   module: {
