fa3e00d96162e2d41c453f5f322d86c00c7e97e1
Dan Mutton <daniel.mutton@gmail.com>
2016-05-23 18:22:32 +1000

    Add textarea option for field

--- frontend/src/components/Field.js
+++ frontend/src/components/Field.js
@@ -1,26 +1,38 @@
 import React, { PropTypes } from 'react';
 
-const Field = ({ children, id, onChange, type = 'text' }) => (
-  <div className="form-group row">
-    <label className="col-sm-2 form-control-label" htmlFor={id}>
+const Field = ({ children, id, onChange, optional, type = 'text' }) => {
+  let input;
+
+  switch (type) {
+  case 'textarea' :
+    input = (
+      <textarea className="form-control" id={id} type={type} onChange={onChange}/>
+      );
+    break;
+  default:
+    input = (
+      <input className="form-control" id={id} required={!optional} type={type} onChange={onChange}/>
+      );
+    break;
+  }
+
+  return (
+    <div className="form-group row">
+      <label className="col-sm-2 form-control-label" htmlFor={id}>
           {children}
-    </label>
-    <div className="col-sm-4">
-      <input
-        className="form-control"
-        id={id}
-        required
-        type={type}
-        onChange={onChange}
-      />
+      </label>
+      <div className="col-sm-4">
+      {input}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Field.propTypes = {
   children: PropTypes.any.isRequired,
   id: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
+  optional: PropTypes.bool,
   type: PropTypes.string,
 };
 
--- frontend/src/components/__tests__/Field_test.js
+++ frontend/src/components/__tests__/Field_test.js
@@ -10,9 +10,15 @@ describe('Field', () => {
     expect(field.find('label').text()).toEqual('some label');
   });
 
-  it('renders an input', () => {
+  it('renders an input when there is no type', () => {
     const field = shallow(<Field id="" onChange={() => {}}>some label</Field>);
 
     expect(field.find('input').length).toEqual(1);
+  });
+
+  it('renders a textarea when the type is textarea', () => {
+    const field = shallow(<Field id="" type="textarea" onChange={() => {}}>some label</Field>);
+
+    expect(field.find('textarea').length).toEqual(1);
   });
 });
