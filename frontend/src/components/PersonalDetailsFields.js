83657da04e939f87d0ab8218df36768a759cf8a1
Jasmine <jquek@thoughtworks.com>
2016-05-31 08:38:40 +0800

    Made address fields more responsive

--- frontend/src/components/PersonalDetailsFields.js
+++ frontend/src/components/PersonalDetailsFields.js
@@ -11,9 +11,11 @@ const PersonalDetailsFields = props =>
       <Field id="firstName" onChange={props.onChange('firstName')}>First Name</Field>
       <Field id="lastName" onChange={props.onChange('lastName')}>Last Name</Field>
       <Field id="street" onChange={props.onChange('street')}>Street</Field>
-      <Field id="city" onChange={props.onChange('city')}>City</Field>
-      <Field id="state" onChange={props.onChange('state')}>State</Field>
-      <Field id="postCode" onChange={props.onChange('postCode')}>Post code</Field>
+      <div className="address">
+        <Field id="city" onChange={props.onChange('city')}>City</Field>
+        <Field id="state" onChange={props.onChange('state')}>State</Field>
+        <Field id="postCode" onChange={props.onChange('postCode')}>Post code</Field>
+      </div>
       <Field id="phone" type="tel" onChange={props.onChange('phone')}>Phone</Field>
       <Field id="email" type="email" onChange={props.onChange('email')}>Email</Field>
     </section>
