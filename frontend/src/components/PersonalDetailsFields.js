c7a2ed53ba99da1e663e1cd4dfceab87b2a44a47
Jasmine <jquek@thoughtworks.com>
2016-05-31 15:53:44 +0800

    Ensuring styles are compatible in mobile view

--- frontend/src/components/PersonalDetailsFields.js
+++ frontend/src/components/PersonalDetailsFields.js
@@ -19,8 +19,10 @@ const PersonalDetailsFields = props =>
       <Field id="phone" type="tel" onChange={props.onChange('phone')}>Phone</Field>
       <Field id="email" type="email" onChange={props.onChange('email')}>Email</Field>
     </section>
-    <input defaultChecked="true" id="optedIn" type="checkbox" onChange={props.onChange('optedIn')}/>
-    <label className="checkbox" htmlFor="optedIn">I want to receive updates and news from Solar Citizens.</label>
+    <label className="checkbox" htmlFor="optedIn">
+      <input defaultChecked="true" id="optedIn" type="checkbox" onChange={props.onChange('optedIn')}/>
+      I want to receive updates and news from Solar Citizens.
+    </label>
   </fieldset>
 );
 
