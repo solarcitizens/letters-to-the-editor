097e3c34fd32632871b27ab6a3458ba4dd19603e
Pam Rucinque <pamela.rucinque@gmail.com>
2016-05-31 10:50:43 +1000

    Magda says typo

--- backend/spec/unit/letterTrasnformerSpec.js
+++ backend/spec/unit/letterTrasnformerSpec.js
@@ -5,7 +5,7 @@ const letterTransformer = require('../../src/parsers/letterTransformer');
 const chai = require('chai');
 const expect = chai.expect;
 
-describe('lettre transformer', () => {
+describe('letter transformer', () => {
   it('should add a signature to the body', () => {
     let letter = letterTransformer.transformLetter(testHelpers.makeLetter());
     expect(letter.body).to.contain(`\n\n--\nDonald Trump\nmakeamericagreat@donaldjtrump.com\n123456789\n725 5th Ave Trump Tower, New York, NY 10022`);
