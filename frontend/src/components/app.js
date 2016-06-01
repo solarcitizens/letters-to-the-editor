400276f7a2084a50ac1188d62ed18512bd35f76e
Jasmine <jquek@thoughtworks.com>
2016-06-01 10:26:26 +0800

    Website now compatible with tablet screen sizes and shortened lorem ipsum

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -11,7 +11,7 @@ const App = () => (
         <h1>Letters to the Editor</h1>
       </div>
       <div className="campaignDescription">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dictum ligula ac mauris vehicula, consectetur accumsan dolor vehicula. Duis eget justo vitae diam consequat rutrum sit amet id nulla. Phasellus cursus nulla eu ante sodales, at fermentum dolor bibendum. Morbi vestibulum vehicula ultrices. Maecenas sed tellus in velit porta convallis et et sapien. Nulla dictum vel magna vitae tincidunt. In nec nulla id neque venenatis pellentesque eget vel leo. Etiam condimentum ipsum vitae ex facilisis ullamcorper. Nulla eget enim ut sem sollicitudin maximus. Vestibulum viverra tristique mi, at laoreet dolor dictum eget. Praesent dignissim, nisi quis pulvinar congue, metus ipsum viverra ex, quis ultricies est risus vel augue. Mauris et tincidunt sapien, scelerisque ornare arcu. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse dictum felis vitae arcu auctor rutrum.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dictum ligula ac mauris vehicula, consectetur accumsan dolor vehicula. Duis eget justo vitae diam consequat rutrum sit amet id nulla. Phasellus cursus nulla eu ante sodales, at fermentum dolor bibendum. Morbi vestibulum vehicula ultrices. Maecenas sed tellus in velit porta convallis et et sapien. Nulla dictum vel magna vitae tincidunt.
       </div>
       <div className="formContainer">
         <div className="formHeader">
