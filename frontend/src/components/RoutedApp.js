0a3d7a702449a87010f32b579842ad9a9b1ec3dd
Magdalena Cervantes Cassel <magdalena.cassel@gmail.com>
2016-06-01 18:04:45 +1000

    Fix import

--- frontend/src/components/RoutedApp.js
+++ frontend/src/components/RoutedApp.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Router, Route, browserHistory } from 'react-router';
-import App from './App.js';
+import App from './App';
 
 const RoutedApp = () => (
   <Router history={browserHistory}>
