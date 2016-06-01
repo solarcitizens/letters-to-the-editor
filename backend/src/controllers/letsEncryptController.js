080ef6f3247dd82fa3f3088c93a8a9e7e8a7c2e4
Robin Doherty <rdoherty@gmail.com>
2016-06-02 08:35:46 +0930

    sabayon seems to be adding _1 to ACME env vars...

--- backend/src/controllers/letsEncryptController.js
+++ backend/src/controllers/letsEncryptController.js
@@ -1,8 +1,8 @@
 'use strict';
 
 let get = (req, res) => {
-  if (req.params.acmeToken === process.env['ACME_TOKEN']) {
-    res.status(200).send(process.env['ACME_KEY']);
+  if (req.params.acmeToken === process.env['ACME_TOKEN_1']) {
+    res.status(200).send(process.env['ACME_KEY_1']);
   } else {
     res.sendStatus(400);
   }
