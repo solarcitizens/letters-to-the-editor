0d16ec154538b26b418d59629f27b809d7201581
Magdalena Cervantes Cassel <magdalena.cassel@gmail.com>
2016-06-02 22:04:51 +1000

    Explanatory text for personal detail fields

--- frontend/src/components/PersonalDetailsFields.js
+++ frontend/src/components/PersonalDetailsFields.js
@@ -23,6 +23,7 @@ const PersonalDetailsFields = props => {
         </div>
         <Field false hasError={isValidationError('phone')} id="phone" type="tel" onChange={props.onChange('phone')}>Phone</Field>
         <Field false hasError={isValidationError('email')} id="email" type="email" onChange={props.onChange('email')}>Email</Field>
+        <div className="explanation">We need to know this information so that editors can contact you.  Please fill in all fields.</div>
       </section>
       <label className="checkbox" htmlFor="optedIn">
         <input defaultChecked="true" id="optedIn" type="checkbox" onChange={props.onChange('optedIn')}/>
