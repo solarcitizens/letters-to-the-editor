13c7f37e9d8a5c6eeb739349102e514bec158054
Jasmine <jquek@thoughtworks.com>
2016-07-09 11:42:26 +0800

    Changed config to reduce webpack bundle size

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,6 @@
+var webpack = require ("webpack");
+
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
   entry: './src/index.js',
   output: {
     path: __dirname + '/../backend/public/javascript',
@@ -31,8 +32,15 @@ module.exports = {
           presets: ['react', 'es2015']
         }
       }
-    ]
-  },
+  ]},
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({minimize: true}),
+  ],
   devServer: { 'hide-modules': true },
   eslint: {
     configFile: '.eslintrc.yml'
