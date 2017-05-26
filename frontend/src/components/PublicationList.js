d03281552663174c79e7be8758f22d646eca97ff
Magdalena Cervantes Cassel <magdalena.cassel@gmail.com>
2017-05-26 12:28:32 -0500

    Fix Firefox bug that prevents submission - Get publication title from props, not from weird event field

--- frontend/src/components/PublicationList.js
+++ frontend/src/components/PublicationList.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const PublicationList = props => (
   <div className="fields">{props.publications.map(title =>
-    <label className="checkbox" key={title}><input key={title} type="checkbox" onChange={props.onChange}/>
+    <label className="checkbox" key={title}><input key={title} type="checkbox" onChange={event => props.onChange(event, title)}/>
     {title}</label>)}</div>
 );
 
